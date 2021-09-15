94b1566ac9a6b3d89c7f83b8d29545d95a16a050
Anusha <anushasunil71201@gmail.com>
2021-09-16 01:54:57 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function reverseString(str)
+{
+    var listOfChars = str.split("");
+    var reverseListOfChars = listOfChars.reverse();
+    var reversedStr = reverseListOfChars.join("");
+    return reversedStr;
+}
