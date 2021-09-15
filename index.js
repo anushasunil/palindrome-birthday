312f2f5b8f509bfb5448cf7b3ed2b257733efebb
Anusha <anushasunil71201@gmail.com>
2021-09-16 02:14:33 +0530

    docs: added check functions

--- index.js
+++ index.js
@@ -5,3 +5,37 @@ function reverseString(str)
     var reversedStr = reverseListOfChars.join("");
     return reversedStr;
 }
+
+function isPalindrome(str)
+{
+    var reversed = reverseString(str);
+    return str === reversed;
+}
+
+function convertDateToString(date)
+{
+    var dateStr = { day: "", month: "", year: ""};
+
+    if(date.day < 10)
+    {
+        dateStr.day = "0" + date.day;
+    }
+    else
+    {
+        dateStr.day = date.day.toString();
+    }
+
+    if(date.month < 10)
+    {
+        dateStr.month = "0" + date.month;
+    }
+    else
+    {
+        dateStr.month = date.month.toString();
+    }
+
+    dateStr.year = date.year.toString();
+
+    return dateStr;
+
+}
